97b67a693b722599949f252787f30544b2758832
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-01-11 07:13:01 -0300

    Merge branch 'feature/Add_readonly_em_campos' into feature/confRemessa
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/main.ts

--- src/resources/usuario/dto/create-usuario.dto.ts
+++ src/resources/usuario/dto/create-usuario.dto.ts
@@ -4,22 +4,22 @@ import { IsEmail, IsNumber, IsString, IsStrongPassword } from 'class-validator';
 export class CreateUsuarioDto {
   @IsNumber()
   @ApiProperty({ example: 1, default: 1, required: true })
-  codigo_status: number;
+  readonly codigo_status: number;
 
   @IsString()
   @ApiProperty({ example: 'João da silva teste', required: true })
-  nome: string;
+  readonly nome: string;
 
   //@IsString()
   @IsEmail()
   @ApiProperty({ example: 'email@email.com', required: true })
-  email: string;
+  readonly email: string;
 
   @IsString()
   @ApiProperty({ example: 'senhaAlterar', required: true })
-  senha: string;
+  readonly senha: string;
 
   @IsNumber()
   @ApiProperty({ example: 1, required: true, default: 1 })
-  nivel: number;
+  readonly nivel: number;
 }
