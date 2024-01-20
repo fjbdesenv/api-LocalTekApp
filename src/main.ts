5e67c03c74418077e1205c1c090bcb3487f939c9
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-01-20 14:46:02 -0300

    Alterando versão para 0.8.1

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function start() {
   const config = new DocumentBuilder()
     .setTitle('Api LocalTekApp')
     .setDescription('Api para consumo de dados LocalTekApp.')
-    .setVersion('0.8.0')
+    .setVersion('0.8.1')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
