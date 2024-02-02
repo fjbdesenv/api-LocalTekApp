06cc9c6f8a6766d2cf563914629526f0bd05a9e0
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-02-02 06:58:34 -0300

    Adicionando configuração de cors

--- src/config/configApp.ts
+++ src/config/configApp.ts
@@ -3,6 +3,7 @@ import { DocumentBuilder } from "@nestjs/swagger";
 export const configApp = new DocumentBuilder()
     .setTitle('Api LocalTekApp')
     .setDescription('Api para consumo de dados LocalTekApp.')
+    .addServer(process.env.APP_HOST || 'http://localhost:5000/')
     .setVersion(process.env.APP_VERSION || '0.0.0')
     .addBearerAuth()
     .build();
