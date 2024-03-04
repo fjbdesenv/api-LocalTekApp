c4af2bb04aabef5afa4fe5660f0628de087e1f94
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-03-04 05:51:30 -0300

    Ajuste: Rwemoção de dados Dto Create AtendimentoArquivo

--- src/resources/atendimento-arquivo/dto/create-atendimento-arquivo.dto.ts
+++ src/resources/atendimento-arquivo/dto/create-atendimento-arquivo.dto.ts
@@ -1,6 +1,1 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class CreateAtendimentoArquivoDto {
-    @ApiProperty({ type: 'string', format: 'binary', required: true })
-    readonly arquivo: Express.Multer.File;
-}
+export class CreateAtendimentoArquivoDto { }
