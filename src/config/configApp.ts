961f264954b5df509a6b81e33de97207dafc6b66
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-03-15 19:45:24 -0300

    Alteração para versão 0.11.4

--- src/config/configApp.ts
+++ src/config/configApp.ts
@@ -4,6 +4,6 @@ export const configApp = new DocumentBuilder()
     .setTitle('Api LocalTekApp')
     .setDescription('Api para consumo de dados LocalTekApp.')
     .addServer(process.env.APP_HOST || 'http://localhost:5000/')
-    .setVersion(process.env.APP_VERSION || '0.0.0')
+    .setVersion(process.env.APP_VERSION || '0.11.4')
     .addBearerAuth()
     .build();
